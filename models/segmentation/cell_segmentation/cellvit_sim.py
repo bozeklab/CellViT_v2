57e920aa252781b82bec3d59ef773b42c43b4431
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-15 23:13:35 +0200

    Added SimViT

--- models/segmentation/cell_segmentation/cellvit_sim.py
+++ models/segmentation/cell_segmentation/cellvit_sim.py
@@ -7,86 +7,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+from .utils import Conv2DBlock, Deconv2DBlock
+
 from cell_segmentation.utils.post_proc_cellvit import DetectionCellPostProcessor
 from models.encoders.VIT.sim_vit import SIMVisionTransformer, unetr_vit_base_patch16
-
-
-class Conv2DBlock(nn.Module):
-    """Conv2DBlock with convolution followed by batch-normalisation, ReLU activation and dropout
-
-    Args:
-        in_channels (int): Number of input channels for convolution
-        out_channels (int): Number of output channels for convolution
-        kernel_size (int, optional): Kernel size for convolution. Defaults to 3.
-        dropout (float, optional): Dropout. Defaults to 0.
-    """
-
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: int = 3,
-        dropout: float = 0,
-    ) -> None:
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride=1,
-                padding=((kernel_size - 1) // 2),
-            ),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(True),
-            nn.Dropout(dropout),
-        )
-
-    def forward(self, x):
-        return self.block(x)
-
-
-class Deconv2DBlock(nn.Module):
-    """Deconvolution block with ConvTranspose2d followed by Conv2d, batch-normalisation, ReLU activation and dropout
-
-    Args:
-        in_channels (int): Number of input channels for deconv block
-        out_channels (int): Number of output channels for deconv and convolution.
-        kernel_size (int, optional): Kernel size for convolution. Defaults to 3.
-        dropout (float, optional): Dropout. Defaults to 0.
-    """
-
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: int = 3,
-        dropout: float = 0,
-    ) -> None:
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                kernel_size=2,
-                stride=2,
-                padding=0,
-                output_padding=0,
-            ),
-            nn.Conv2d(
-                in_channels=out_channels,
-                out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride=1,
-                padding=((kernel_size - 1) // 2),
-            ),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(True),
-            nn.Dropout(dropout),
-        )
-
-    def forward(self, x):
-        return self.block(x)
 
 
 def interpolate_pos_embed(model, checkpoint_model):
